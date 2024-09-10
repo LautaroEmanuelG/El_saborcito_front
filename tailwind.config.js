5e473517a52ae18632fd9a29ec0b87f7442b0c62
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-09-10 20:58:51 -0300

    Feat (Web , Buscador) : Creados componentes y estilados

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#E11D48',
+        secondary: '#C2BCBC',
+        blanco: '#f2f2f2',
+        negro: '#2f2f2f',
+      },
+    },
   },
   plugins: [],
-}
+};
