c2ba7fb98496e4993b93bf8a80567d925b456894
ErosMariotti <145143267+ErosMariotti@users.noreply.github.com>
2024-09-11 20:45:01 -0300

    feat (iconos) : puse todos los iconos svg con funciones

---
+++ src/components/iconos/IconoCirculo.tsx
@@ -0,0 +1,9 @@
+const IconoLogoSaborcito: React.FC = () => {
+    return (
+        <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="15" cy="15" r="12.5" fill="white" stroke="#E11D48" stroke-width="5"/>
+        </svg>
+    );
+  };
+  
+  export default IconoLogoSaborcito;
