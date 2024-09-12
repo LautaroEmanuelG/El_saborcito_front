79179ebef3d79fbe072b67a9bd531c1f1ef20a19
pablosoor <pablo.osorio@alumnos.frm.utn.edu.ar>
2024-09-11 22:17:37 -0300

    update web

--- src/views/user/Web.tsx
+++ src/views/user/Web.tsx
@@ -8,7 +8,7 @@ export const Web = ({}: Props) => {
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 w-full">
       <Header totalItems={0} />
       <div className="container mx-auto px-4 md:px-6 py-4 flex flex-col min-h-screen w-full">
-        <ListaCategorias />
+        <ListaCategorias /> 
       </div>
     </div>
   );
