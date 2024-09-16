1aa5f7dc907c747beb2a914f1b62800fb1babb6b
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-09-15 21:06:13 -0300

    Fix (Iconos) : Corregidos nombres de componentes

--- src/components/iconos/IconoBilletes.tsx
+++ src/components/iconos/IconoBilletes.tsx
@@ -1,15 +1,32 @@
-const IconoLogoSaborcito: React.FC = () => {
-    return (
-        <svg width="40" height="40" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg" >
-        <rect width="40" height="40" fill="url(#pattern0_233_1339)"/>
-        <defs>
-        <pattern id="pattern0_233_1339" patternContentUnits="objectBoundingBox" width="1" height="1">
-        <use transform="scale(0.0104167)"/>
+const IconoBilletes: React.FC = () => {
+  return (
+    <svg
+      width="40"
+      height="40"
+      viewBox="0 0 40 40"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <rect
+        width="40"
+        height="40"
+        fill="url(#pattern0_233_1339)"
+      />
+      <defs>
+        <pattern
+          id="pattern0_233_1339"
+          patternContentUnits="objectBoundingBox"
+          width="1"
+          height="1">
+          <use transform="scale(0.0104167)" />
         </pattern>
-        <image id="image0_233_1339" width="96" height="96"/>
-        </defs>
-        </svg>
-    );
-  };
-  
-  export default IconoLogoSaborcito;
+        <image
+          id="image0_233_1339"
+          width="96"
+          height="96"
+        />
+      </defs>
+    </svg>
+  );
+};
+
+export default IconoBilletes;
--- src/components/iconos/IconoLogoSaborcito.tsx
+++ src/components/iconos/IconoLogoSaborcito.tsx
@@ -1,12 +1,29 @@
 const IconoLogoSaborcito: React.FC = () => {
   return (
-    <svg width="29" height="28" viewBox="0 0 29 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect width="29" height="28" fill="url(#pattern0_95_305)" />
+    <svg
+      width="29"
+      height="28"
+      viewBox="0 0 29 28"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg">
+      <rect
+        width="29"
+        height="28"
+        fill="url(#pattern0_95_305)"
+      />
       <defs>
-        <pattern id="pattern0_95_305" patternContentUnits="objectBoundingBox" width="1" height="1">
+        <pattern
+          id="pattern0_95_305"
+          patternContentUnits="objectBoundingBox"
+          width="1"
+          height="1">
           <use transform="matrix(0.0100575 0 0 0.0104167 0.0172414 0)" />
         </pattern>
-        <image id="image0_95_305" width="96" height="96" />
+        <image
+          id="image0_95_305"
+          width="96"
+          height="96"
+        />
       </defs>
     </svg>
   );
