ae2faacc045bcb227e7039d9a81b83f550ec8410
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-09-25 13:02:44 -0300

    Feat (Hover) : Corregidos pasos de props para manejar hover correctamente

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,48 +2,52 @@ import { Buscador } from './Buscador';
 import IconoCarrito from '../iconos/IconoCarrito';
 import IconoLoggin from '../iconos/IconoLoggin';
 import IconoLogoSaborcito from '../iconos/IconoLogoSaborcito';
+import { useState } from 'react';
 
 type Props = {
   totalItems: number;
 };
 
 export const Header = ({ totalItems }: Props) => {
+  const [hoverLogin, setHoverLogin] = useState(false);
+  const [hoverCarrito, setHoverCarrito] = useState(false);
+
   return (
     <header className="bg-primary flex w-full text-primary-foreground py-4 shadow-md">
       <div className="container mx-auto flex items-center justify-between px-4 md:px-6">
         <a
           href="#"
           className="flex items-center gap-4">
-            <IconoLogoSaborcito />
-          <span className="text-2xl font-bold text-white">
-            El Saborcito
-          </span>
+          <IconoLogoSaborcito />
+          <span className="text-2xl font-bold text-white">El Saborcito</span>
         </a>
         <div className="relative flex-1 max-w-md">
           <Buscador />
         </div>
-        <div className="flex items-center gap-4">
-          <button className="rounded-full hover:bg-blanco">
-            <IconoLoggin />
-            {/* <span className="text-blanco font-bold">Login</span> */}
+        <div className="flex items-center gap-4 ">
+          <button
+            className="relative flex items-center justify-center gap-4 w-10 h-10 rounded-full hover:bg-blanco"
+            onMouseEnter={() => setHoverLogin(true)}
+            onMouseLeave={() => setHoverLogin(false)}>
+            <IconoLoggin color={hoverLogin ? '#E11D48' : 'white'}/>
           </button>
           {totalItems > 0 ? (
-            <div className="relative flex items-center justify-center gap-4 w-10 h-10 rounded-full hover:bg-blanco">
-              <button className="rounded-full">
-                <IconoCarrito />
-                {/* <span className="text-blanco font-bold">Car</span> */}
-                <div className="absolute text-blanco -top-3 -right-3 bg-primary text-primary-foreground rounded-full px-2 py-1 text-xs font-bold hover:bg-blanco hover:text-primary ">
-                  {totalItems}
-                </div>
-              </button>
-            </div>
-          ): (
-            <div className="relative flex items-center justify-center gap-4 w-10 h-10 rounded-full hover:bg-blanco">
-              <button className="rounded-full">
-                <IconoCarrito />
-                {/* <span className="text-blanco font-bold">Car</span> */}
-              </button>
-            </div>
+            <button
+              className="relative flex items-center justify-center gap-4 w-10 h-10 rounded-full hover:bg-blanco"
+              onMouseEnter={() => setHoverCarrito(true)}
+              onMouseLeave={() => setHoverCarrito(false)}>
+              <IconoCarrito color={hoverCarrito ? '#E11D48' : 'white'} />
+              <div className="absolute text-blanco -top-3 -right-3 bg-primary text-primary-foreground rounded-full px-2 py-1 text-xs font-bold hover:bg-blanco hover:text-primary ">
+                {totalItems}
+              </div>
+            </button>
+          ) : (
+            <button
+              className="relative flex items-center justify-center gap-4 w-10 h-10 rounded-full hover:bg-blanco"
+              onMouseEnter={() => setHoverCarrito(true)}
+              onMouseLeave={() => setHoverCarrito(false)}>
+              <IconoCarrito color={hoverCarrito ? '#E11D48' : 'white'} />
+            </button>
           )}
         </div>
       </div>
--- src/components/iconos/IconoLoggin.tsx
+++ src/components/iconos/IconoLoggin.tsx
@@ -1,4 +1,8 @@
-const IconoLoggin: React.FC = () => {
+interface IconoLogginProps {
+  color: string;
+}
+
+const IconoLoggin: React.FC<IconoLogginProps> = ({ color}) => {
   return (
     <svg
       width="45"
@@ -8,14 +12,14 @@ const IconoLoggin: React.FC = () => {
       xmlns="http://www.w3.org/2000/svg">
       <path
         d="M30 32V30C30 28.9391 29.5786 27.9217 28.8284 27.1716C28.0783 26.4214 27.0609 26 26 26H20C18.9391 26 17.9217 26.4214 17.1716 27.1716C16.4214 27.9217 16 28.9391 16 30V32"
-        stroke="white"
+        stroke={color}
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
       />
       <path
         d="M23 22C25.2091 22 27 20.2091 27 18C27 15.7909 25.2091 14 23 14C20.7909 14 19 15.7909 19 18C19 20.2091 20.7909 22 23 22Z"
-        stroke="white"
+        stroke={color}
         stroke-width="2"
         stroke-linecap="round"
         stroke-linejoin="round"
