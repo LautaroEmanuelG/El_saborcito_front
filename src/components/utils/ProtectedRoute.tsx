3e94c3024f8dab3151e83288a2d187c0a0bf6c88
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-11-07 03:30:16 -0300

    Feat (LoginModal, ProtectedRoute, Main): Update navigation to redirect to historial, enhance role-based access control, and restructure admin routes

--- src/components/utils/ProtectedRoute.tsx
+++ src/components/utils/ProtectedRoute.tsx
@@ -3,7 +3,7 @@ import { Navigate } from 'react-router-dom';
 import { ReactNode } from 'react';
 
 const ProtectedRoute = ({ children }: { children: ReactNode }) => {
-  const isAuthenticated = true; // Aquí deberías verificar si el usuario está autenticado
+  const isAuthenticated = localStorage.getItem('rol') === 'Admin'; // Verifica si el rol es Admin
 
   return isAuthenticated ? children : <Navigate to="/" />;
 };
