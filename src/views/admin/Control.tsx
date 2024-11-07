2c0303289739142de2e50a37d01266dc471b12a2
LautaroEmanuelG <lgonzalez.sag98@gmail.com>
2024-11-07 02:59:26 -0300

    Feat (Control): Add date filtering and transaction type selection for accounting entries

--- src/views/admin/Control.tsx
+++ src/views/admin/Control.tsx
@@ -6,6 +6,9 @@ export const Control: React.FC = () => {
   const [asientosContables, setAsientosContables] = useState<any[]>([]);
   const [libroDiario, setLibroDiario] = useState<any[]>([]);
   const [libroMayor, setLibroMayor] = useState<any[]>([]);
+  const [selectedTipo, setSelectedTipo] = useState<string>('MP');
+  const [startDate, setStartDate] = useState<string>('');
+  const [endDate, setEndDate] = useState<string>('');
 
   useEffect(() => {
     fetchInitialData();
@@ -29,11 +32,20 @@ export const Control: React.FC = () => {
 
       const debe = ['MP', 'EFECTIVO', 'CRIPTO'].includes(transaccion.tipo)
         ? detalles.reduce((acc: number, detalle: any) => acc + detalle.precio, 0)
-        : detalles.reduce((acc: number, detalle: any) => acc + detalle.costo, 0);
+        : detalles.reduce(
+            (acc: number, detalle: any) => acc + detalle.costo * detalle.cantidad,
+            0
+          );
 
       const haber = ['MP', 'EFECTIVO', 'CRIPTO'].includes(transaccion.tipo)
-        ? detalles.reduce((acc: number, detalle: any) => acc + detalle.costo + detalle.ganancia, 0)
-        : detalles.reduce((acc: number, detalle: any) => acc + detalle.costo, 0);
+        ? detalles.reduce(
+            (acc: number, detalle: any) => acc + detalle.costo + detalle.ganancia,
+            0
+          )
+        : detalles.reduce(
+            (acc: number, detalle: any) => acc + detalle.costo * detalle.cantidad,
+            0
+          );
 
       return {
         id: transaccion.id,
@@ -74,10 +86,12 @@ export const Control: React.FC = () => {
       });
       return acc;
     }, {});
-    setLibroMayor(Object.entries(mayor).map(([producto, valores]) => ({
-      producto,
-      ...valores,
-    })));
+    setLibroMayor(
+      Object.entries(mayor).map(([producto, valores]) => ({
+        producto,
+        ...valores,
+      }))
+    );
   };
 
   const formatCurrency = (amount: number) => {
@@ -96,8 +110,33 @@ export const Control: React.FC = () => {
       minute: '2-digit',
       second: '2-digit',
     };
-    return new Intl.DateTimeFormat('es-ES', options).format(new Date(dateString));
+    return new Intl.DateTimeFormat('es-ES', options).format(
+      new Date(dateString)
+    );
   };
+
+  const handleTipoChange = (tipo: string) => {
+    setSelectedTipo(tipo);
+  };
+
+  const handleStartDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setStartDate(event.target.value);
+  };
+
+  const handleEndDateChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value);
+    setEndDate(event.target.value);
+  };
+
+  const filterByDate = (asiento: any) => {
+    const asientoDate = new Date(asiento.fecha).toISOString().split('T')[0]; // Asegura que esté en formato 'YYYY-MM-DD'
+    const start = startDate ? new Date(startDate).toISOString().split('T')[0] : null;
+    const end = endDate ? new Date(endDate).toISOString().split('T')[0] : null;
+  
+    return (!start || asientoDate >= start) && (!end || asientoDate <= end);
+  };
+  
 
   return (
     <div className="container mx-auto p-6">
@@ -105,7 +144,7 @@ export const Control: React.FC = () => {
 
       {/* Mostrar Asientos Contables */}
       <div className="mb-4">
-        <h2 className="text-2xl font-semibold">Asientos Contables</h2>
+        <h2 className="text-2xl font-semibold pb-4">Libro Diario</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {asientosContables.map(asiento => (
             <div
@@ -115,52 +154,43 @@ export const Control: React.FC = () => {
                 Asiento ID: {asiento.id}
               </h3>
               <div className="text-lg text-gray-600">
-                <span className="font-medium">Fecha:</span> {formatDate(asiento.fecha)}
+                <span className="font-medium">Fecha:</span>{' '}
+                {formatDate(asiento.fecha)}
               </div>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Debe:</span> {formatCurrency(asiento.debe)}
-              </div>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Haber:</span> {formatCurrency(asiento.haber)}
-              </div>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Caja:</span> {asiento.tipo}
-              </div>
-              <div className="mt-2 text-sm text-gray-500">
-                <h4 className="font-semibold">Detalles:</h4>
-                <ul>
-                  {asiento.detalles.map((detalle: any, index: number) => (
-                    <li key={index}>
-                      {detalle.cantidad} x {detalle.producto} - Precio: {formatCurrency(detalle.precio)} - Costo: {formatCurrency(detalle.costo)}
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Mostrar Libro Diario */}
-      <div className="mb-4">
-        <h2 className="text-2xl font-semibold">Libro Diario</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {libroDiario.map((entry, index) => (
-            <div
-              key={index}
-              className="p-4 border border-gray-300 rounded-lg shadow-md bg-white">
-              <h3 className="text-xl font-semibold text-gray-700 mb-2">
-                {formatDate(entry.fecha)}
-              </h3>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Descripción:</span> {entry.descripcion}
-              </div>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Debe:</span> {formatCurrency(entry.debe)}
-              </div>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Haber:</span> {formatCurrency(entry.haber)}
-              </div>
+              {asiento.tipo === 'MERCADERIA' ? (
+                <>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Debe:</span>{' '}
+                    {formatCurrency(asiento.debe)}
+                  </div>
+                  
+                  <div className="text-lg text-gray-600">
+                    <span className="pl-4 font-medium">- Caja:</span> MERCADERIA
+                  </div>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Haber:</span>{' '}
+                    {formatCurrency(asiento.haber)}
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Debe:</span>{' '}
+                    {formatCurrency(asiento.debe)}
+                  </div>
+                  <div className="text-lg text-gray-600">
+                    <span className="pl-4 font-medium">- Caja:</span> {asiento.tipo}
+                  </div>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Haber:</span>{' '}
+                    {formatCurrency(asiento.haber)}
+                  </div>
+                  
+                  <div className="text-lg text-gray-600">
+                    <span className="pl-4 font-medium">- Caja:</span> MERCADERIA
+                  </div>
+                </>
+              )}
             </div>
           ))}
         </div>
@@ -168,23 +198,98 @@ export const Control: React.FC = () => {
 
       {/* Mostrar Libro Mayor */}
       <div className="mb-4">
-        <h2 className="text-2xl font-semibold">Libro Mayor</h2>
+        <h2 className="text-2xl font-semibold pb-4">Libro Mayor</h2>
+        <div className="flex justify-center mb-4">
+          <button
+            className={`px-4 py-2 mx-2 rounded ${selectedTipo === 'MP' ? 'bg-primary text-white' : 'bg-gray-200'}`}
+            onClick={() => handleTipoChange('MP')}
+          >
+            MP
+          </button>
+          <button
+            className={`px-4 py-2 mx-2 rounded ${selectedTipo === 'EFECTIVO' ? 'bg-primary text-white' : 'bg-gray-200'}`}
+            onClick={() => handleTipoChange('EFECTIVO')}
+          >
+            EFECTIVO
+          </button>
+          <button
+            className={`px-4 py-2 mx-2 rounded ${selectedTipo === 'CRIPTO' ? 'bg-primary text-white' : 'bg-gray-200'}`}
+            onClick={() => handleTipoChange('CRIPTO')}
+          >
+            CRIPTO
+          </button>
+          <button
+            className={`px-4 py-2 mx-2 rounded ${selectedTipo === 'MERCADERIA' ? 'bg-primary text-white' : 'bg-gray-200'}`}
+            onClick={() => handleTipoChange('MERCADERIA')}
+          >
+            MERCADERIA
+          </button>
+        </div>
+        <div className="flex justify-center mb-4">
+          <input
+            type="date"
+            value={startDate}
+            onChange={handleStartDateChange}
+            className="px-4 py-2 mx-2 border rounded"
+          />
+          <input
+            type="date"
+            value={endDate}
+            onChange={handleEndDateChange}
+            className="px-4 py-2 mx-2 border rounded"
+          />
+        </div>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {libroMayor.map((entry, index) => (
-            <div
-              key={index}
+          {asientosContables
+            .filter(asiento => asiento.tipo === selectedTipo)
+            .filter(filterByDate)
+            .map((asiento) => (
+              <div
+              key={asiento.id}
               className="p-4 border border-gray-300 rounded-lg shadow-md bg-white">
               <h3 className="text-xl font-semibold text-gray-700 mb-2">
-                Producto: {entry.producto}
+                Asiento ID: {asiento.id}
               </h3>
               <div className="text-lg text-gray-600">
-                <span className="font-medium">Debe:</span> {formatCurrency(entry.debe)}
+                <span className="font-medium">Fecha:</span>{' '}
+                {formatDate(asiento.fecha)}
               </div>
-              <div className="text-lg text-gray-600">
-                <span className="font-medium">Haber:</span> {formatCurrency(entry.haber)}
-              </div>
+              {asiento.tipo === 'MERCADERIA' ? (
+                <>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Debe:</span>{' '}
+                    {formatCurrency(asiento.debe)}
+                  </div>
+                  
+                  <div className="text-lg text-gray-600">
+                    <span className="pl-4 font-medium">- Caja:</span> MERCADERIA
+                  </div>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Haber:</span>{' '}
+                    {formatCurrency(asiento.haber)}
+                  </div>
+                </>
+              ) : (
+                <>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Debe:</span>{' '}
+                    {formatCurrency(asiento.debe)}
+                  </div>
+                  <div className="text-lg text-gray-600">
+                    <span className="pl-4 font-medium">- Caja:</span> {asiento.tipo}
+                  </div>
+                  <div className="text-lg text-gray-600">
+                    <span className="font-medium">Haber:</span>{' '}
+                    {formatCurrency(asiento.haber)}
+                  </div>
+                  
+                  <div className="text-lg text-gray-600">
+                    <span className="pl-4 font-medium">- Caja:</span> MERCADERIA
+                  </div>
+                </>
+              )}
             </div>
-          ))}
+            ))}
         </div>
       </div>
     </div>
