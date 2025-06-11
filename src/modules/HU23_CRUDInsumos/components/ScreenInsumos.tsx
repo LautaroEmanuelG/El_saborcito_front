a31f41d06ca11ab74a1b68fce781a88f7f16cd86
Lautaro Gonzalez <98829008+LautaroEmanuelG@users.noreply.github.com>
2025-06-11 15:20:45 -0300

    Update ScreenInsumos.tsx

--- src/modules/HU23_CRUDInsumos/components/ScreenInsumos.tsx
+++ src/modules/HU23_CRUDInsumos/components/ScreenInsumos.tsx
@@ -131,7 +131,7 @@ export const ScreenInsumos = () => {
     <div className="container mx-auto p-4">
       <div className="flex w-full justify-end items-center mb-6">
         <button
-          className="bg-primary hover:bg-primarydark text-white font-bold py-2 px-4 rounded-lg shadow-lg transition-all duration-200 transform hover:scale-105"
+          className="bg-primary hover:bg-primarydark text-blanco font-bold py-2 px-4 rounded-lg shadow-lg transition-all duration-200 transform hover:scale-105"
           onClick={handleAdd}
           disabled={showDeleted}
           title={
