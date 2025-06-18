d58aefff2b112a48ab5a15e9867e57544b214b9e
pabloosoor <osoriopablo03@gmail.com>
2025-06-18 11:57:39 -0300

    Mercado pago implementacion correcta con redireccion (#216)
    Co-authored-by: Lautaro Gonzalez <98829008+LautaroEmanuelG@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -23,9 +23,9 @@ import ScreenPromociones from './modules/HU25_Promociones/components/ScreenPromo
 import { Recepcion } from './modules/HU14_Recepcion/components/Recepcion';
 import { Delivery } from './modules/HU16_Delivery/components/Delivery';
 import ScreenCompraIngredientes from './modules/HU24_CompraIngredientes/components/ScreenCompraIngredientes';
-import { CallbackPage } from './app/views/CallbackPage';
-import PedidoExitoso from './pages/PedidoExitoso';
-import { PerfilClienteDashboard } from './modules/HU3_Perfil_Cliente/components/PerfilClienteDashboard';
+import { CallbackPage } from './app/views/CallbackPage.tsx';
+import { HistorialPedidosCliente } from './modules/HU13_MisPedidos/index.ts';
+import { PerfilClienteDashboard } from './modules/HU3_Perfil_Cliente/components/PerfilClienteDashboard.tsx';
 import ScreenStockInsumos from './modules/HU25_ControlStockInsumos/components/ScreenStockInsumos';
 import { GestionEmpleados } from './modules/HU4_Registro_Empleado';
 import { PerfilEmpleadoDashboard } from './modules/HU6_Perfil_Empleado/components/PerfilEmpleadoDashboard';
